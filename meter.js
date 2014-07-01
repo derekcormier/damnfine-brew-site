aee6ddf57185df684b4db5115bf67f0e89b0c3b8
Derek Cormier <derek@derekcormier.com>
2014-07-01 13:38:51 -0400

    Got scrolling between labels with values working

--- meter.js
+++ meter.js
@@ -1,36 +1,112 @@
 $(document).ready(function()
 {
-	var abv = 6;
-	var ibu = 83;
-	var srm = 22;
+	labelNum = 0;
+
+	var abv = [6, 6.8, 6.8, 6.8, 5.2, "?", 6.8];
+	var ibu = [83, 26, 26, 26, 35, "?", 52];
+	var srm = [30, 25, 25, 25, 6, "?", 8];
+	var highlight = ["#", "#", "#", "#", "#", "#", "#"];
 	
-	updateMeter();
+	var sliderCount = $('#label-slider ul li').length;
+	var sliderWidth = $('#label-slider ul li').width();
+	var sliderHeight = $('#label-slider ul li').height();
+	var sliderUlWidth = sliderCount * sliderWidth;
 	
-	function updateMeter() {
+	$('#label-slider').css({ width: sliderWidth, height: sliderHeight });
+	
+	$('#label-slider ul').css({ width: sliderUlWidth, marginLeft: - sliderWidth });
+	
+    $('#label-slider ul li:last-child').prependTo('#label-slider ul');
+	
+	updateMeter(abv[0], ibu[0], srm[0]);
+	
+	function updateMeter(abv, ibu, srm) {
 		
 		var meterwidth = $('.scale').width();
 		var abvLimit = 12;
 		var ibuLimit = 120;
 		var srmLimit = 35;
 		
-		updateValue('#abv', 6, abvLimit);
-		updateValue('#ibu', 83, ibuLimit);
-		updateValue('#srm', 22, srmLimit);
+		$('#labelNum').text(labelNum);
+		
+		updateValue('#abv', abv, abvLimit);
+		updateValue('#ibu', ibu, ibuLimit);
+		updateValue('#srm', srm, srmLimit);
 		
 		function updateValue(meterID, newValue, valueLimit) {
-			var currentValue = $(meterID + ' div .meter-value').text();
-		
-			$(meterID + ' .needle').stop(true).animate({left: (meterwidth * (newValue/valueLimit) - 5)}, {duration: 750});
+			if (newValue == "?") {
+				$(meterID + ' .needle').stop(true).animate({left: ((meterwidth * .5) - 5)}, {duration: 750});
+				$(meterID + ' div .meter-value').text("??");
+			} else {
 			
-			$({someValue: currentValue}).animate({someValue: newValue}, {
-				duration: 750,
-				step: function() {
-					$(meterID + ' div .meter-value').text(Math.floor(this.someValue + ((currentValue - newValue)/750)));
-				},
-				complete : function(){
-					$(meterID + ' div .meter-value').text(newValue);
+				if ($(meterID + ' div .meter-value').text() == "??") {
+					if (meterID == '#abv') {
+						var currentValue = (abvLimit/2);
+					} else if (meterID == '#ibu') {
+						var currentValue = (ibuLimit/2);
+					} else {
+						var currentValue = (srmLimit/2);
+					}
+				} else {
+					var currentValue = $(meterID + ' div .meter-value').text();
 				}
-			});
+			
+				$(meterID + ' .needle').stop(true).animate({left: (meterwidth * (newValue/valueLimit) - 5)}, {duration: 750});
+				
+				$({someValue: currentValue}).animate({someValue: newValue}, {
+					duration: 750,
+					step: function() {
+						$(meterID + ' div .meter-value').text(Math.floor(this.someValue + ((currentValue - newValue)/750)));
+					},
+					complete : function(){
+						$(meterID + ' div .meter-value').text(newValue);
+					}
+				});
+			}
 		}
 	}
+	
+	function moveLeft() {
+		
+		if (labelNum != 0) {
+			labelNum -= 1;
+		} else {
+			labelNum = (sliderCount - 1);
+		}
+		
+		updateMeter(abv[labelNum], ibu[labelNum], srm[labelNum]);
+	
+        $('#label-slider ul').animate({
+            left: + sliderWidth
+        }, 300, function () {
+            $('#label-slider ul li:last-child').prependTo('#label-slider ul');
+            $('#label-slider ul').css('left', '');
+        });
+    };
+
+    function moveRight() {
+
+		if (labelNum != (sliderCount - 1)) {
+			labelNum += 1;
+		} else {
+			labelNum = 0;
+		}
+	
+		updateMeter(abv[labelNum], ibu[labelNum], srm[labelNum]);
+	
+        $('#label-slider ul').animate({
+            left: - sliderWidth
+        }, 300, function () {
+            $('#label-slider ul li:first-child').appendTo('#label-slider ul');
+            $('#label-slider ul').css('left', '');
+        });
+    };
+
+    $('a#slider-previous').click(function () {
+        moveLeft();
+    });
+
+    $('a#slider-next').click(function () {
+        moveRight();
+    });
 });
