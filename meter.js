7d9bcab70b2b588d0bc332893523c796b1ff3655
Derek Cormier <derek@derekcormier.com>
2014-07-01 09:40:32 -0400

    put meter update into function (got rid of tons of duplicate code). Zeroed the meters.

--- meter.js
+++ meter.js
@@ -2,58 +2,35 @@ $(document).ready(function()
 {
 	var abv = 6;
 	var ibu = 83;
-	var srm = 70;
+	var srm = 22;
 	
 	updateMeter();
 	
 	function updateMeter() {
 		
-		var meterwidth = $(".scale").width();
+		var meterwidth = $('.scale').width();
 		var abvLimit = 12;
 		var ibuLimit = 120;
-		var srmLimit = 150;
+		var srmLimit = 35;
 		
-		var currentABV = $("#abv div .meter-value").text();
-		var currentIBU = $("#ibu div .meter-value").text();
-		var currentSRM = $("#srm div .meter-value").text();
+		updateValue('#abv', 6, abvLimit);
+		updateValue('#ibu', 83, ibuLimit);
+		updateValue('#srm', 22, srmLimit);
 		
-		$("#abv .needle").stop(true).animate({left: (meterwidth * (abv/abvLimit) - 5)}, {duration: 750});
+		function updateValue(meterID, newValue, valueLimit) {
+			var currentValue = $(meterID + ' div .meter-value').text();
 		
-		$({someValue: currentABV}).animate({someValue: abv}, {
-			duration: 750,
-			step: function() { // called on every step
-				// Update the element's text with value:
-				$('#abv div .meter-value').text(Math.floor(this.someValue + ((currentABV - abv)/750)));
-			},
-			complete : function(){
-				$('#abv div .meter-value').text(abv);
-			}
-		});
-		
-		$("#ibu .needle").stop(true).animate({left: (meterwidth * (ibu/ibuLimit) - 5)}, {duration: 750});
-		
-		$({someValue: currentIBU}).animate({someValue: ibu}, {
-			duration: 750,
-			step: function() { // called on every step
-				// Update the element's text with value:
-				$('#ibu div .meter-value').text(Math.floor(this.someValue + ((currentIBU - ibu)/750)));
-			},
-			complete : function(){
-				$('#ibu div .meter-value').text(ibu);
-			}
-		});
-		
-		$("#srm .needle").stop(true).animate({left: (meterwidth * (srm/srmLimit) - 5)}, {duration: 750});
-		
-		$({someValue: currentSRM}).animate({someValue: srm}, {
-			duration: 750,
-			step: function() { // called on every step
-				// Update the element's text with value:
-				$('#srm div .meter-value').text(Math.floor(this.someValue + ((currentSRM - srm)/750)));
-			},
-			complete : function(){
-				$('#srm div .meter-value').text(srm);
-			}
-		});
+			$(meterID + ' .needle').stop(true).animate({left: (meterwidth * (newValue/valueLimit) - 5)}, {duration: 750});
+			
+			$({someValue: currentValue}).animate({someValue: newValue}, {
+				duration: 750,
+				step: function() {
+					$(meterID + ' div .meter-value').text(Math.floor(this.someValue + ((currentValue - newValue)/750)));
+				},
+				complete : function(){
+					$(meterID + ' div .meter-value').text(newValue);
+				}
+			});
+		}
 	}
 });
