25dc05d6ca871273a32c29a00d5fc34a5616b310
Derek Cormier <derek@derekcormier.com>
2014-07-15 12:32:28 -0400

    added new beer insert scripts, changed page width to 960px.

--- dbscripts/create_beers.sql
+++ dbscripts/create_beers.sql
@@ -3,10 +3,11 @@ USE damnfinebrew_db;
 CREATE TABLE beers (
 	id INT NOT NULL AUTO_INCREMENT,
 	name VARCHAR(128) NOT NULL,
-	label_url VARCHAR(128) NOT NULL,
+	label_image VARCHAR(50) NOT NULL,
 	abv DECIMAL(2,1),
 	ibu SMALLINT,
 	srm TINYINT,
-	highlight_color VARCHAR(7),
-	description VARCHAR(10000)	
+	highlight_color VARCHAR(7) NOT NULL,
+	description VARCHAR(10000) NOT NULL,
+	PRIMARY KEY( id )
 ) ENGINE=InnoDB;
