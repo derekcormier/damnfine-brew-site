1eb6fa5fe16dc70609181c71d93b66d0a2f64591
Derek Cormier <derek@derekcormier.com>
2014-09-05 16:50:08 -0400

    added fonts to open source list, made comments not overflow

--- website/open-source-attributions.php
+++ website/open-source-attributions.php
@@ -25,24 +25,26 @@
 				<div id="inner-content">
 					<h1>Open Source Attributions</h1>
 					
-					<h2>PHP</h2>
+					<h2>Software</h2>
+					
+					<h3>PHP</h3>
 					<p>This product includes PHP software, freely available from <a href="http://www.php.net/software/">http://www.php.net/software/</a>.</p>
 					<p><a href="http://php.net/license/3_01.txt">Full License</a></p>
 					
-					<h2>MySQL</h2>
+					<h3>MySQL</h3>
 					<p>You may not use this file except in compliance with the GNU General Public License (the "License"). You may obtain a copy of the License at <a href="http://www.gnu.org/licenses/gpl-faq.html">http://www.gnu.org/licenses/gpl-faq.html</a>. Software distributed under the License is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for the specific language governing rights and limitations under the License.</p>
 					
-					<h2>JQuery</h2>
+					<h3>JQuery</h3>
 					<p>jQuery projects are released under the terms of the <a href="http://en.wikipedia.org/wiki/MIT_License">MIT license</a>.</p>
 					<p>The MIT License is simple and easy to understand and it places almost no restrictions on what you can do with a jQuery project.</p>
 					<p>You are free to use any jQuery project in any other project (even commercial projects) as long as the copyright header is left intact.</p>
 					
-					<h2>JQuery UI</h2>
+					<h3>JQuery UI</h3>
 					<p>jQuery projects are released under the terms of the <a href="http://en.wikipedia.org/wiki/MIT_License">MIT license</a>.</p>
 					<p>The MIT License is simple and easy to understand and it places almost no restrictions on what you can do with a jQuery project.</p>
 					<p>You are free to use any jQuery project in any other project (even commercial projects) as long as the copyright header is left intact.</p>
 					
-					<h2>Parsedown</h2>
+					<h3>Parsedown</h3>
 					<p>The MIT License (MIT)</p>
 					<p>Copyright (c) 2013 Emanuil Rusev, erusev.com</p>
 					<p>Permission is hereby granted, free of charge, to any person obtaining a copy of
@@ -59,6 +61,14 @@
 					COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
 					IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 					CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.</p>
+					
+					<h2>Fonts</h2>
+					
+					<h3>Open Sans</h3>
+					<p><a href="http://www.apache.org/licenses/LICENSE-2.0.html">Apache 2.0</a></p>
+					
+					<h3>Sansita One</h3>
+					<p><a href="http://scripts.sil.org/cms/scripts/page.php?item_id=OFL_web">SIL Open Font License, 1.1</a></p>
 				</div>
 				
 				<?php include "./php/footer.php" ?>
